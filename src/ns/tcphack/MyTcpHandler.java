564d659756ba391b4c34f7710ba0ef1058d47a67
Noah Goldsmid <noahgoldsmid@live.nl>
2015-03-19 17:04:42 +0100

    Push

--- src/ns/tcphack/MyTcpHandler.java
+++ src/ns/tcphack/MyTcpHandler.java
@@ -15,14 +15,19 @@ class MyTcpHandler extends TcpHandler {
         NetworkLayer nLayer = new NetworkLayer(this, IPv6.myIP);
         TransportLayer tLayer = new TransportLayer(nLayer, IPv6.remoteIP, 7719);
         tLayer.connect();
+        long lastMsg = System.currentTimeMillis();
 		while (!done) {
             String result = tLayer.recv();
             if (result != null) {
+                lastMsg = System.currentTimeMillis();
                 System.out.println("Packet: " + result);
             }
             if (tLayer.connected() && !sent) {
                 sent = true;
                 tLayer.send("GET / HTTP/1.0\n\n");
+            }
+            if (System.currentTimeMillis() - lastMsg >= 10*1000) {
+                tLayer.close();
             }
 		}   
 	}
